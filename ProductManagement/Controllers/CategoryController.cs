67023ba06f0e1fd23064f3c7d52714f882fc7f0e
Sangamesh21101996 <sangameshhalli2110@gmail.com>
2024-06-24 00:00:32 +0530

    Completed Category CRUD 24-June-24

--- ProductManagement/Controllers/CategoryController.cs
+++ ProductManagement/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProductManagement.Data.ApplicationDb.SqlServer;
+using ProductManagement.Models.Category;
 
 namespace ProductManagement.Controllers
 {
@@ -18,6 +19,112 @@ namespace ProductManagement.Controllers
             var getCategoryList = _dbcontext.CategoriesEntity.ToList();
 
             return View(getCategoryList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryEntity ctgy)
+        {
+            //we can custome validation using ModelState
+
+            if(ctgy.CategoryName.Any(char.IsDigit))
+            {
+                ModelState.AddModelError("CategoryName", "Name must not contain Digits");
+            }
+            //If All validation are satisfied then go for db save
+            if (ModelState.IsValid)
+            {
+                _dbcontext.CategoriesEntity.Add(ctgy);
+                _dbcontext.SaveChanges();
+                //We can Add Temp Data this will remain in temp memory and will lost after rendering 
+                TempData["success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+
+            //retrun the empty view 
+            return View();
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int ctgId)
+        {
+            if(ctgId == 0)
+            {
+                //We can pass here Not found view 
+                return NotFound();
+            }
+
+            var getCategory = _dbcontext.CategoriesEntity.SingleOrDefault(ctg=> ctg.CategoryId == ctgId);
+            if (getCategory == null)
+            {
+                //We can pass view or error view 
+                return NotFound();
+            }
+
+            return View(getCategory);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CategoryEntity ctg)
+        {
+            //we can custome validation using ModelState
+
+            if (ctg.CategoryName.Any(char.IsDigit))
+            {
+                ModelState.AddModelError("CategoryName", "Name must not contain Digits");
+            }
+            //If All validation are satisfied then go for db save
+            if (ModelState.IsValid)
+            {
+                _dbcontext.CategoriesEntity.Update(ctg);
+                _dbcontext.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+
+            //retrun the empty view 
+            return View();
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int ctgId)
+        {
+            if (ctgId == 0)
+            {
+                //We can pass here Not found view 
+                return NotFound();
+            }
+
+            var getCategory = _dbcontext.CategoriesEntity.SingleOrDefault(ctg => ctg.CategoryId == ctgId);
+            if (getCategory == null)
+            {
+                //We can pass view or error view 
+                return NotFound();
+            }
+
+            return View(getCategory);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult Remove(CategoryEntity ctg)
+        {
+            if(ctg==null)
+            {
+                return NotFound();
+            }
+
+            _dbcontext.CategoriesEntity.Remove(ctg);
+            _dbcontext.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
         }
     }
 }
--- ProductManagement/Models/Category/CategoryEntity.cs
+++ ProductManagement/Models/Category/CategoryEntity.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProductManagement.Models.Category
@@ -9,7 +10,11 @@ namespace ProductManagement.Models.Category
         [Key]
         public int CategoryId { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(50)]
         public string CategoryName { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be 1-100")]
         public int DisplayOrder { get; set; }
 
     }
